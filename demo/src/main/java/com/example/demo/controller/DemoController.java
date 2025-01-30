f97a35329d543e0df6b357906cdc393631cf346b
I'm Yong Tack <aldjzk@naver.com>
2025-01-30 16:49:29 +0900

    #4 swagger add

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.DemoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,13 +15,16 @@ public class DemoController {
         this.demoService = demoService;
     }
 
+    @Operation(summary = "테스트 API 2", description = "고정값 'ssss'을 반환합니다.")
     @GetMapping("/test2")
     public String sayHello2() {
         return demoService.getHelloMessage("ssss");
     }
 
+    @Operation(summary = "테스트 API", description = "Query Parameter로 전달된 name을 받아 메시지를 반환합니다.")
     @GetMapping("/test")
-    public String sayHello(@RequestParam(required = false, defaultValue = "Guest") String name) {
+    public String sayHello(
+            @Parameter(description = "사용자의 이름", example = "Zippity") @RequestParam(required = false, defaultValue = "Guest") String name) {
         return demoService.getHelloMessage(name);
     }
 }
