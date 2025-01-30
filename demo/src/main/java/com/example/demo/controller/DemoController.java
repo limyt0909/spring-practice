181e59122868da28c3a5dc8da11852035dff7729
I'm Yong Tack <aldjzk@naver.com>
2025-01-30 17:11:05 +0900

    #5 redis connection test

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.DemoService;
+
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/demo")
-public class DemoController {
+@RequestMapping("/demo")public class DemoController {
 
     private final DemoService demoService;
 
@@ -15,16 +15,19 @@ public class DemoController {
         this.demoService = demoService;
     }
 
-    @Operation(summary = "테스트 API 2", description = "고정값 'ssss'을 반환합니다.")
-    @GetMapping("/test2")
-    public String sayHello2() {
-        return demoService.getHelloMessage("ssss");
+        @Operation(summary = "테스트 API 2", description = "redis save test.")
+
+    @PostMapping("/redis/save")
+    public String saveToRedis(@RequestParam String key, @RequestParam String value) {
+        demoService.saveToRedis(key, value);
+        return "Saved!";
     }
 
-    @Operation(summary = "테스트 API", description = "Query Parameter로 전달된 name을 받아 메시지를 반환합니다.")
-    @GetMapping("/test")
-    public String sayHello(
-            @Parameter(description = "사용자의 이름", example = "Zippity") @RequestParam(required = false, defaultValue = "Guest") String name) {
-        return demoService.getHelloMessage(name);
+        @Operation(summary = "테스트 API 1", description = "redis get test2")
+
+    @GetMapping("/redis/get")
+    public String getFromRedis(@RequestParam String key) {
+        String value = demoService.getFromRedis(key);
+        return value != null ? "Value: " + value : "Key not found!";
     }
 }
--- demo/src/main/java/com/example/demo/service/DemoService.java
+++ demo/src/main/java/com/example/demo/service/DemoService.java
@@ -1,13 +1,24 @@
 package com.example.demo.service;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
 @Service
 public class DemoService {
-    
-    //더미 서비스 1
-    public String getHelloMessage(String str) {
-        return str;
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public DemoService(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
     }
 
-    //더미 서비스 2
+    public void saveToRedis(String key, String value) {
+        redisTemplate.opsForValue().set(key, value, Duration.ofMinutes(10)); // 10분 유지
+    }
 
+    public String getFromRedis(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
 }
